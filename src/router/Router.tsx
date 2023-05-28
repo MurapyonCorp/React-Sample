508ae90b372bb800d31c96416e48d1a9ebd27b59
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2023-05-28 17:22:09 +0900

    ルーティング作成中

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,19 +2,18 @@ import { FC, memo } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
-import { Home } from "../components/pages/Home";
-import { UserManagement } from "../components/pages/UserManagement";
-import { Setting } from "../components/pages/Setting";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { homeRoutes } from "./HomeRoutes";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home">
-        <Route index element={<Home />} />
-        <Route path="user_management" element={<UserManagement />} />
-        <Route path="setting" element={<Setting />} />
+      <Route path="/home" element={<HeaderLayout /> }>
+        {homeRoutes.map((route) => (
+          <Route key={route.path} index={route.index} path={route.path} element={route.children} />
+        ))}
       </Route>
       <Route path="*" element={<Page404 />} />
     </Routes>
