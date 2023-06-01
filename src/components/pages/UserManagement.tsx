bd1d9a36795fdceb80bdb97f633f6db7a0e03821
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2023-06-01 12:56:25 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,13 @@
 import { FC, memo } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard imageUrl="https://source.unsplash.com/featured" userName="りょう" fullName="Murashige ryo" />
+      </WrapItem>
+    </Wrap>
+  );
 });
