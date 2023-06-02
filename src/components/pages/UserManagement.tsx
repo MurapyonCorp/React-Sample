516e96450b528b3478eb2690130e28f4116d2e7a
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2023-06-02 13:10:22 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,30 @@
-import { FC, memo } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { FC, memo, useEffect } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers()
+  
+  useEffect(() => getUsers(), [])
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard imageUrl="https://source.unsplash.com/featured" userName="りょう" fullName="Murashige ryo" />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+            {users.map((user) => (
+              <WrapItem key={user.id}>
+                <UserCard imageUrl="https://source.unsplash.com/featured" userName={user.username} fullName={user.name} />
+              </WrapItem>
+            ))}
+        </Wrap>
+      )}
+    </>
   );
 });
